26c2bd44a9cdd771e71938e764c4563dfa345ac9
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-12-08 23:04:34 +0000

    first commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+/opt/amazon/openmpi/bin/mpirun -np 2 -H \
+    ip-172-31-88-106:1,ip-172-31-76-23:1 \
+    -bind-to none -map-by slot \
+    -x LD_LIBRARY_PATH=/opt/amazon/openmpi/lib:$LD_LIBRARY_PATH \
+    -x HOROVOD_LOG_LEVEL=TRACE \
+    -tag-output \
+    -mca pml ob1 \
+    -mca btl ^openib \
+    -mca btl_tcp_if_exclude lo,docker0 \
+    /home/ubuntu/anaconda3/envs/horovod_dev/bin/python /home/ubuntu/horovod-examples/pytorch/pytorch_mnist.py
