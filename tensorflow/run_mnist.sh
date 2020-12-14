bca99f00b374da22b082e01130ee902b727d63d0
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-12-14 13:48:46 +0000

    add HOROVOD_HOSTS

--- tensorflow/run_mnist.sh
+++ tensorflow/run_mnist.sh
@@ -3,6 +3,7 @@
 /opt/amazon/openmpi/bin/mpirun -np 2 -H \
     ip-172-31-88-106:1,ip-172-31-76-23:1 \
     -bind-to none -map-by slot \
+    -x HOROVOD_HOSTS="172.31.88.106 172.31.76.23" \
     -x LD_LIBRARY_PATH=/opt/amazon/openmpi/lib:$LD_LIBRARY_PATH \
     -x HOROVOD_LOG_LEVEL=TRACE \
     -tag-output \
